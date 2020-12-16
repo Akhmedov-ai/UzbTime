33706ca175a1c36d303bf67a163d82e325a67671
Akhmedov-ai <axmedov.aslbek2708@gmail.com>
2020-12-16 11:06:46 +0500

    settings

--- practice/settings.py
+++ practice/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
